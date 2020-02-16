bd552042e3292e09b113b4ed5907a7445ecbf797
Ingimar <ingimareys93@gmail.com>
2020-02-16 12:41:36 +0100

    cleaned up the list

--- js.js
+++ js.js
@@ -1,38 +1,93 @@
-window.addEventListener("DOMContentLoaded", event => {
-  fetch("students1991.json")
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      data.forEach(e => {
-        const template = document.querySelector(".studentlist-template")
-          .content;
-        const clone = template.cloneNode(true);
-        const name = e.fullname;
-        const house = e.house;
-        const sname = clone.querySelector(".studentdetails");
-        sname.innerHTML = name;
-        sname.dataset.house = house;
-        const innername = clone.querySelector(".Nameinner");
-        innername.innerHTML = name;
-        const img = clone.querySelector(".img");
-        img.src = `images/` + house + `.svg`;
-        const houseinner = clone.querySelector(".houseinner");
-        houseinner.innerHTML += house;
-        const houses = document.querySelectorAll(".list");
-        const modal = clone.querySelector(".seemore");
-        modal.setAttribute("data-houseTheme", `` + e.house + ``);
-        houses.forEach(e => {
-          if (e.classList.contains(house)) {
-            e.appendChild(clone);
-          }
-        });
-      });
+// JavaScript Document
+window.addEventListener("DOMContentLoaded", getData);
+function getData() {
+  fetch("https://petlatkea.dk/2020/hogwarts/students.json")
+    .then(res => res.json())
+    .then(handleData)
+    .then(appendFunc);
+}
+const Student = {
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  house: "",
+  gender: ""
+};
+studentArray = [];
+let number = 0;
+function handleData(myData) {
+  myData.forEach(e => {
+    number++;
+    const names = e.fullname.split(" ");
+    const index = names.indexOf("");
+    if (index !== -1) names.splice(index, 1);
+    const house = e.house.split(" ");
+    const index1 = house.indexOf("");
+    if (index1 !== -1) house.splice(index1, 1);
+    const gender = e.gender.split(" ");
+    const index2 = gender.indexOf("");
+    if (index2 !== -1) gender.splice(index2, 1);
+
+    let studentIndex = Object.create(Student);
+    studentIndex.firstName =
+      names[0].charAt(0).toUpperCase() + names[0].substring(1).toLowerCase();
+    if (names.length == 3) {
+      studentIndex.middleName =
+        names[1].charAt(0).toUpperCase() + names[1].substring(1).toLowerCase();
+      studentIndex.lastName =
+        names[2].charAt(0).toUpperCase() + names[2].substring(1).toLowerCase();
+    }
+    if (names.length == 2) {
+      studentIndex.lastName =
+        names[1].charAt(0).toUpperCase() + names[1].substring(1).toLowerCase();
+    }
+    studentIndex.house =
+      house[0].charAt(0).toUpperCase() + house[0].substring(1).toLowerCase();
+    studentIndex.gender =
+      gender[0].charAt(0).toUpperCase() + gender[0].substring(1).toLowerCase();
+    studentArray.push(studentIndex);
+  });
+}
+
+function appendFunc() {
+  studentArray.forEach(e => {
+    const template = document.querySelector(".studentlist-template").content;
+    const clone = template.cloneNode(true);
+    const sname = clone.querySelector(".studentdetails");
+
+    const firstname = e.firstName;
+    const middleName = e.middleName;
+    const lastname = e.lastName;
+    sname.innerHTML = firstname + `&nbsp` + middleName + `&nbsp` + lastname;
+
+    const house = e.house;
+    sname.dataset.house = house;
+
+    const innername = clone.querySelector(".Nameinner");
+    innername.innerHTML = firstname + `&nbsp` + middleName + `&nbsp` + lastname;
+
+    const img = clone.querySelector(".img");
+    img.src = `images/` + house + `.svg`;
+
+    const gender = clone.querySelector(".gender");
+    gender.innerHTML = e.gender;
+
+    const houseinner = clone.querySelector(".houseinner");
+    houseinner.innerHTML += house;
+
+    const houses = document.querySelectorAll(".list");
+
+    const modal = clone.querySelector(".seemore");
+    modal.setAttribute("data-houseTheme", `` + e.house + ``);
+    houses.forEach(e => {
+      if (e.classList.contains(house)) {
+        e.appendChild(clone);
+      }
     });
-});
+  });
+}
 const images = document.querySelectorAll(".house1");
 let reply = function() {
-  console.log(this.dataset.house);
   images.forEach(e => {
     if ((e.dataset.active = "active")) {
       e.setAttribute("data-active", "");
@@ -58,9 +113,9 @@ images.forEach(e => {
 
 document.body.addEventListener("click", function(event) {
   if (event.target.classList.contains("studentdetails")) {
-    var target = event.target;
+    const target = event.target;
     target.classList.add("hide");
-    var title = event.target.nextElementSibling;
+    const title = event.target.nextElementSibling;
 
     if (title.classList.contains("hide")) {
       title.classList.remove("hide");
@@ -72,8 +127,8 @@ document.body.addEventListener("click", function(event) {
 });
 document.body.addEventListener("click", function(event) {
   if (event.target.classList.contains("x")) {
-    var title = event.target.parentElement;
-    var prev = title.previousElementSibling;
+    const title = event.target.parentElement;
+    const prev = title.previousElementSibling;
     if (title.classList.contains("hide")) {
       title.classList.remove("hide");
     } else {
